2541682d2b5dca18e688a4273950448d0f20baa6
Stark <ankit566kota@gmail.com>
2026-04-22 12:02:39 +0530

    feat: add accounts app and JWT login endpoint

---
+++ backend/accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class LoginAPI(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                "access": str(refresh.access_token),
+                "refresh": str(refresh)
+            })
+
+        return Response({"error": "Invalid credentials"}, status=401)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import LoginAPI
+# from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', LoginAPI.as_view()),
 ]
